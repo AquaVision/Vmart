e2bfc0b3c0db65bf5904bbc0a643a8158e932462
dinidu sandaruwan <iamdinidu@gmail.com>
2013-07-14 13:15:12 +0530

    Shopping cart basic fuctions added

---
+++ application/controllers/Web_cart.php
@@ -0,0 +1,65 @@
+<?php
+class Web_cart extends CI_Controller{
+    
+    function add(){
+    
+        $this->load->model('Product_model');
+        $product = $this->Product_model->get($this->input->post('id'));
+        $data = array(
+               'id'      => $this->input-post('id'),
+               'qty'     => 1, 
+               'price'   => $product->price,
+               'name'    => $product->title
+            );
+        
+        $this->cart->insert($data);
+        redirect('Vmart');
+
+    
+    
+    }
+    function total(){
+        
+        echo $this->cart->total();
+    }
+    
+
+    function show(){
+        
+        $cart = $this->cart->contents();
+        echo '<pre>';
+        print_r($cart);
+    }
+    
+    function remove(){
+        
+        $data = array(
+        'rowid' => 'd0c00b4e4b747d8475d1c93ff8067138',
+        'qty'   => 0
+        );
+        $this->cart->update($data);
+        echo 'item removed';
+    }
+
+
+
+
+    function destroy(){
+        $this->cart->destroy();
+        echo 'destroied cart!!!';
+    }
+    
+    
+    
+    function test($id){
+        $this->load->model('Product_model');
+        $test_result = $this->Product_model->get($id);
+        var_dump($test_result);
+        
+    }
+    
+}
+
+?>  
+
+
