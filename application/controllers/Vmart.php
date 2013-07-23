e139022626f93f14189a6f70e662cdf17406c610
imal hasaranga <imaa95@gmail.com>
2013-07-23 13:31:44 +0530

    nothign done i think :P

--- application/controllers/Vmart.php
+++ application/controllers/Vmart.php
@@ -1,42 +1,35 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
 
 class Vmart extends CI_Controller {
 
     private $data = array();
-    
-    public function __construct()
-    {
+
+    public function __construct() {
         parent::__construct();
         $this->load->model('Land_model');
-    } 
-    
+    }
+
     public function index() {
         $this->data['top_items'] = $this->Land_model->get_top_items();
 //        print_r($this->data['top_items']);
 //        die();
-        $this->load->view('landingpage',$this->data);
+        $this->load->view('landingpage', $this->data);
     }
-    
-    public function get_orders(){
-        if (!islogedUser()){
-            
+
+    public function get_orders() {
+        if (!islogedUser()) {
             echo 'not logged in';
-            
-        }  else {
-        
-        $userid = getUserID();
-        $this->load->model('product_model');
-        $orders = $this->product_model->get_new_orders($userid);
-        echo $orders;
+        } else {
+            $userid = getUserID();
+            $this->load->model('product_model');
+            $orders = $this->product_model->get_new_orders($userid);
+            echo $orders;
         }
-        
-       
-        
-       }
+    }
 
 }
 
-        
-       
-        
 ?>
